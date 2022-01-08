8533bb6d08d862adce884606a59caa9f60826914
EC2 Default User <ec2-user@ip-172-31-25-170.ap-northeast-1.compute.internal>
2022-01-08 11:35:12 +0000

    1/8

---
+++ for.py
@@ -0,0 +1,12 @@
+for num in range(0, 10, 1):
+    print("Hello World")
+    
+animals = ["dog", "cat", "bird"]
+for animal in animals:
+    print(animal)
+    
+for i in range(1, 31, 1):
+    if i % 3 == 0:
+        print("三の倍数です")
+    else:
+        print (i)
--- restaurants_searcher.py
+++ restaurants_searcher.py
@@ -1,2 +1,10 @@
-restaurants = "[]"
-response = "hugahuga"
+def write_date_to_csv():
+    
+    restaurants = "[]"
+    response = "hugahuga"
+    if "error" in response["results"]:
+       return print("エラーが発生しました！")
+    
+    return print(restaurants)
+    
+write_date_to_csv()
