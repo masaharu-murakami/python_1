dedb190c8d96d72d2ef7088e442b5f716d694dc4
MasaharuMurakami <89374344+masaharu-murakami@users.noreply.github.com>
2023-08-30 11:36:02 +0900

    11:35

--- 20230829.py
+++ 20230829.py
@@ -6,3 +6,22 @@ money += 5000
 
 # 変数 money の値を出力してください
 print(money)
+
+money = 2000
+print(money)
+
+# 変数 money に 5000 を足して、変数 money を上書きしてください
+money += 5000
+
+# 変数 money の値を出力してください
+print(money)
+
+age = 24
+# age を用いて「私は24歳です」と出力してください
+print('私は' + str(age) +'歳です' )
+
+count = '5'
+# count に 1 を足した値を出力してください
+print(int(count) + 1)
+
+
